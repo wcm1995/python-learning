1796cb75506e2e26416275cff4e98f9e90c1eade
wcm <1304234393@qq.com>
2018-06-21 22:59:48 +0800

    上传文件

---
+++ flask project/wangye/config.py
@@ -0,0 +1,16 @@
+import os
+
+DEBUG = True
+
+SECRET_KEY = os.urandom(24)
+
+DIALECT = 'mysql'
+DRIVER = 'mysqldb'
+USERNAME = 'root'
+PASSWORD = '0706520jason'
+HOST = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'wangye'
+
+SQLALCHEMY_DATABASE_URI="{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT, DRIVER, USERNAME, PASSWORD,
+                                                                HOST, PORT, DATABASE)
